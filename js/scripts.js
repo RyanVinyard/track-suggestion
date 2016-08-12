a48fac4ba2abdf3864306f970a49598a7d630af4
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2016-08-12 11:13:03 -0700

    add more questions as well as branching

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,47 @@ $(document).ready(function() {
     var username = $("#username").val();
     $("#yourname").hide("slow");
     $("#question1").show("slow");
+
   $("form#question1").submit(function() {
     event.preventDefault();
-    var whichtrack = $("#answer1")
-    if (whichtrack === "front-end") {
+    var answer1 = $("input:radio[name=answer1]:checked").val();
+    if (answer1 === "front-end") {
       $("#question1").hide("slow");
       $("#question2f").show("slow");
-    } else if (whichtrack === "back-end") {
+    } else if (answer1 === "back-end") {
       $("#question1").hide("slow");
       $("#question2b").show("slow");
     }
+
+  $("form#question2f").submit(function() {
+    event.preventDefault();
+    var answer2f = $("input:radio[name=answer2f]:checked").val();
+    $("#question2f").hide("slow");
+    $("#question3f").show("slow");
+  });
+
+  $("form#question3f").submit(function() {
+    event.preventDefault();
+    var answer3f = $("input:radio[name=answer3f]:checked").val();
+    if (answer3f === "backendforms") {
+      $("#question3f").hide("slow");
+      $("#php").show("slow");
+    } else if (answer3f === "backendlittle") {
+      $("#question3f").hide("slow");
+      $("#css").show("slow");
+    } else if (answer3f === "backendnone") {
+      $("#question3f").hide("slow");
+      $("#design").show("slow");
+    }
+
+  $("form#question2b").submit(function() {
+    event.preventDefault();
+    var answer2b = $("input:radio[name=answer2b]:checked").val();
+
+  });
+  });
+
+
   });
 });
 });
