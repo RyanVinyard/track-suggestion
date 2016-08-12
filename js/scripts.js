b891d009940fa4ca78600d279c0be93ced9f56e8
Ryan Vinyard <rav.ryanvinyard@gmail.com>
2016-08-12 11:58:13 -0700

    have a full scale of questions, not all of which work

--- js/scripts.js
+++ js/scripts.js
@@ -35,11 +35,46 @@ $(document).ready(function() {
     } else if (answer3f === "backendnone") {
       $("#question3f").hide("slow");
       $("#design").show("slow");
-    }
+    };
 
   $("form#question2b").submit(function() {
     event.preventDefault();
     var answer2b = $("input:radio[name=answer2b]:checked").val();
+    if (answer2b === "workforself") {
+      $("#question2b").hide("slow");
+      $("#question3bsticktheman").show("slow");
+    } else if (answer2b === "workforcompany") {
+      $("#question2b").hide("slow");
+      $("#question3b-corporate-slave").show("slow");
+    };
+
+  $("form#question3bsticktheman").submit(function() {
+    event.preventDefault();
+    var answer3bsticktheman = $("input:radio[name=answer3bsticktheman]:checked").val();
+    if (answer3bsticktheman === "application") {
+      $("#question3bsticktheman").hide("slow");
+      $("#java").show("slow");
+    } else if (answer3bsticktheman === "software") {
+      $("#question3bsticktheman").hide("slow");
+      $("#csharp").show("slow");
+    };
+
+  $("form#question3bcorporateslave").submit(function() {
+    event.preventDefault();
+    var answer3bcorporateslave = $("input:radio[name=answer3bcorporateslave]:checked").val();
+    if (answer3bcorporateslave === "business-software") {
+      $("#question3bcorporateslave").hide("slow");
+      $("#csharp").show("slow");
+    } else if (answer3bcorporateslave === "mobileapps") {
+      $("#question3bcorporateslave").hide("slow");
+      $("#java").show("slow");
+    } else if (answer3bcorporateslave === "websites") {
+      $("#question3bcorporateslave").hide("slow");
+      $("#php").show("slow");
+    };
+    console.log(username);
+  });
+});
 
   });
   });
